bc1a31fb6a4864decee6e415a44ebe69200d0eb1
GiovanniKaaijk <kaaijkg@gmail.com>
2019-11-12 20:20:47 +0100

    started working on the timeline, changed dct:spatial to broader for more results

--- index.js
+++ index.js
@@ -12,39 +12,20 @@ const cityToCountry = (fetchurl) => {
 }
 cityToCountry('https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json')
 
-const rendermap = (d3) => {
-        
-        d3.json('http://enjalot.github.io/wwsd/data/world/world-110m.geojson')
-            .then(json => {
-                json.features.forEach((feature, i) => {
-                    countryArray.push(feature.properties.name);
-                    dataCount.push(feature)
-                    dataCount[i].properties.count = 0;
-                });
-            })
+const rendermapLayout = (d3) => {     
+    d3.json('http://enjalot.github.io/wwsd/data/world/world-110m.geojson')
+        .then(json => {
+            json.features.forEach((feature, i) => {
+                countryArray.push(feature.properties.name);
+                dataCount.push(feature)
+                dataCount[i].properties.count = 0;
+            });
+        })
 };
-rendermap(d3);
+rendermapLayout(d3);
   
 //https://www.youtube.com/watch?v=Qw6uAg3EO64
 
-//Github CMDA 
-const url =
-    "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-02/sparql";
-//Note that the query is wrapped in es6 template strings to allow for easy copy pasting
-const query = `
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX dc: <http://purl.org/dc/elements/1.1/>
-PREFIX dct: <http://purl.org/dc/terms/>
-
-SELECT ?cho ?placeName WHERE {
-    ?place skos:prefLabel ?placeName .
-    ?cho dct:spatial ?place ;
-            dct:created ?year .
-    } 
-    LIMIT 10000
-`;
-
-let results = [];
 
 const runQuery = (url, query) => {
     // Call the url with the query attached, output data
@@ -52,8 +33,8 @@ const runQuery = (url, query) => {
     .then(res => res.json())
     .then(json => {
         let highestCount = 0;
-
-        results = json.results.bindings;
+        //change results placename to match more items in the following forEach. If item is not in city JSON -> delete
+        let results = json.results.bindings;
         results.forEach((result) => {
             if(!countryArray.includes(result.placeName.value)){
                 cities.forEach((city) => {
@@ -62,7 +43,14 @@ const runQuery = (url, query) => {
                     }
                 });
             }
+            if(!countryArray.includes(result.placeName.value)){
+                let index = results.indexOf(result);
+                results.splice(index, 1)
+            }
         });
+        console.log(countryArray)
+        console.log(results)
+        //check if any placename is equal to one of the countries, if so -> country count + 1
         results.forEach(result => {
             if(countryArray.includes(result.placeName.value)) {
                 dataCount.forEach((counter) => {
@@ -75,23 +63,59 @@ const runQuery = (url, query) => {
                 });
             }
         })
-        const worldMap = d3.geoNaturalEarth1(); //comes in different styles on d3
+        const worldMap = d3.geoNaturalEarth1(); //natural earth gives a good realistic view of the map
         const pathCreator = d3.geoPath().projection(worldMap);
         svg.append('path')
             .attr('class', 'sphere')
             .attr('d', pathCreator({type: 'Sphere'}));
-        console.log(dataCount);
-        let scale = d3.scaleLinear()
+        let scaleColor = d3.scaleLinear()
             .domain([0, highestCount])
-            .range(['#c6c6c6', '#033033']);
+            .range(['#e6e6e6', '#033033']);
+        let scaleStroke = d3.scaleLinear()
+            .domain([0, highestCount])
+            .range(['#000000', '#ffffff'])
         svg.selectAll('path')
             .data(dataCount) 
             .enter()
             .append('path')
                 .attr('d', pathCreator)
                 .attr('class', 'country')
-                .style('fill', function (d) { return scale(d.properties.count) })
+                .style('fill', (d) => scaleColor(d.properties.count))
+                .style('stroke', (d) => scaleStroke(d.properties.count))
 
     })
 };
+
+
+const url = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-02/sparql";
+const query = `
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX dct: <http://purl.org/dc/terms/>
+
+SELECT ?cho ?placeName WHERE {
+
+    ?placeBroader skos:prefLabel ?placeName .
+    ?cho dct:spatial ?place ;
+            dct:created ?year .
+    ?place skos:broader ?placeBroader .
+    } 
+    LIMIT 50000
+`;
+
 runQuery(url, query);
+
+const timeLine = () => {
+    let nodes = document.querySelectorAll('.timeline div')
+    nodes.forEach(element => {
+        let content = element.textContent;
+        content = content.split("-")
+        let selectedTime = {
+            firstValue: content[0],
+            secondValue: content[1]
+        } 
+        console.log(selectedTime)
+    })
+}
+
+timeLine();
